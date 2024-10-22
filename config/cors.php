8ac53244d98a477fe0b141bf1fc082e397dfa51c
ASIA-890-Dao-Ngoc_Tuyen <tuyendn@ans-asia.com>
2024-10-22 17:16:53 +0700

    fix

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+   'allowed_origins' => explode(',', env('FRONTEND_URLS', 'http://localhost:3000')),
 
     'allowed_origins_patterns' => [],
 
@@ -27,7 +27,7 @@ return [
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 3600,
 
     'supports_credentials' => true,
 
